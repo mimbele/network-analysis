69b02fd2f968d6750bd4233b7e9d1d4092709abc
mimbele <mrym.ndr@gmail.com>
2021-04-02 14:11:06 +0430

    meh

--- infectDublin/covertTXTtoCSV.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-read_file = pd.read_csv(r'/home/mimbele/PycharmProjects/pythonProject/infectDublin_edges.txt')
-read_file.to_csv(r'/home/mimbele/PycharmProjects/pythonProject/infectDublin_edges_CSV.csv', index=None)
